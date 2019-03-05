0890ae780886e4a5d37f2bf2abd39448db993a8d
Anton Skatt <k8asnakeload@gmail.com>
2019-03-05 18:46:41 +0100

    awda

---
+++ Assets/Rotator.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class Rotator : MonoBehaviour
+{  
+    public float speed;
+}
+
+class RotatorSystem : ComponentSystem
+{
+    struct Components
+    {
+        public Rotator rotator;
+        public Transform transform;
+    }
+
+    // Will run every frame
+    protected override void OnUpdate()
+    {
+        float deltaTime = Time.deltaTime;
+
+        foreach (var e in GetEntities<Components>())
+        {
+            e.transform.Rotate(0f, e.rotator.speed * deltaTime, 0f);
+        }
+    }
+}
